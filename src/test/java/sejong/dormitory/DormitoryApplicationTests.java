838d9dea6acab2f14fb6f99afdd1d735e507f0b3
JongHun <pjh575157@gmail.com>
2022-09-19 23:22:52 +0900

    프로젝트 생성

---
+++ src/test/java/sejong/dormitory/DormitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package sejong.dormitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
