f0fb9ea142b94a91daf03f3c75b764c86d079e46
JongHun <pjh575157@gmail.com>
2022-09-20 22:20:53 +0900

    2022/09/20 MemberContoller 오류 수정/ @PostMapping 추가 수정: @GetMapping 인자로 Model을 받아 addAttribute로 처리 @PostMapping 이후 MemberFormDto로 받은 후 memberService로 member 저장 처리

--- src/main/java/sejong/dormitory/controller/MemberContoller.java
+++ src/main/java/sejong/dormitory/controller/MemberContoller.java
@@ -1,11 +1,15 @@
 package sejong.dormitory.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sejong.dormitory.dto.MemberFormDto;
+import sejong.dormitory.entity.Member;
 import sejong.dormitory.service.MemberService;
 
 @Controller
@@ -13,9 +17,19 @@ import sejong.dormitory.service.MemberService;
 @RequiredArgsConstructor
 public class MemberContoller {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/new")
-    public String memberForm(@ModelAttribute("memberFormDto")MemberFormDto memberFormDto){
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+    }
+
+    @PostMapping("/new")
+    public String memberForm(MemberFormDto memberFormDto){
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
     }
 }
