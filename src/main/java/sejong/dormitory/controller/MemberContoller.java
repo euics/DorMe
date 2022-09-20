6eb0f00c0d7339bcdd065f81cda37d4e3c2e864f
JongHun <pjh575157@gmail.com>
2022-09-20 22:37:22 +0900

    2022/09/20 MemberController 유효성 검사 추가 회원가입이 성공이면 메인 페이지로 리다이렉트 시켜주고, 회원 정보 검증 및 중복회원 가입 조건에 의해 실패한다면 다시 회원 가입 페이지로 돌아가 실패 이유를 화면에 출력

--- src/main/java/sejong/dormitory/controller/MemberContoller.java
+++ src/main/java/sejong/dormitory/controller/MemberContoller.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import sejong.dormitory.dto.MemberFormDto;
 import sejong.dormitory.entity.Member;
 import sejong.dormitory.service.MemberService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/members")
@@ -26,9 +29,19 @@ public class MemberContoller {
     }
 
     @PostMapping("/new")
-    public String memberForm(MemberFormDto memberFormDto){
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+    public String memberForm(@Valid MemberFormDto memberFormDto,
+                             BindingResult bindingResult,
+                             Model model){
+        if(bindingResult.hasFieldErrors())
+            return "member/memberForm";
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
