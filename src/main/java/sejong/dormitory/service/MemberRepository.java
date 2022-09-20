3887fd9bab50d757b11dbac0d6a14e6711c397c5
JongHun <pjh575157@gmail.com>
2022-09-20 21:40:06 +0900

    2022/09/20 MemberService 생성 -비즈니스 로직을 담당하는 서비스 계층 클래스에 @Transactional 어노테이션을 선언 -이미 가입된 회원의 경우 IllegalStateException 예외를 발생

--- src/main/java/sejong/dormitory/service/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package sejong.dormitory.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sejong.dormitory.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
