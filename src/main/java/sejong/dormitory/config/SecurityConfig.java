32f50bddf668d9971118c7dfad558d1b94593bb8
JongHun <pjh575157@gmail.com>
2022-09-20 23:06:19 +0900

    2022/09/20 SecurityConfig 로그인/로그아웃 구현 로그인/로그아웃 성공, 실패, URL 설정
    auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
    -> Spring Security에서 인증은 AuthenticationManager를 통해 이루어지며 AuthenticationManagerBuilder가 AuthenticationManager를 생성합니다.
    userDetailService를 구현하고 있는 객체로 memberService를 지정해주며, 비밀번호 암호화를 위해 passwordEncoder를 지정해줍니다.

--- src/main/java/sejong/dormitory/config/SecurityConfig.java
+++ src/main/java/sejong/dormitory/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package sejong.dormitory.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import sejong.dormitory.service.MemberService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    MemberService memberService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/members/login")
                 .defaultSuccessUrl("/")
                 .failureUrl("/members/login/error")
-                .usernameParameter("email")
+                .usernameParameter("username")
 
                 .and()
 
@@ -33,5 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService)
+                .passwordEncoder(passwordEncoder());
     }
 }
