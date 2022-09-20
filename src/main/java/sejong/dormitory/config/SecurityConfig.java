a4e40c44da4d4627a88bc50a4d81ea5cafa975f9
JongHun <pjh575157@gmail.com>
2022-09-20 10:50:22 +0900

    2022/09/20 SecurityConfig 클래스로 인증&인가 API 생성 서버가 기동되면 스프링 시큐리티의 초기화 작업 및 보안 설정 별도의 설정이나 구현을 하지 않아도 기본적인 웹 보안 기능이 현재 시스템에 연동 1. 모든 요청은 인증이 되어야 자원에 접근이 가능하다. 2. 인증 방식은 폼 로그인 방식과 httpBasic로그인 방식을 제공한다. 3. 기본 로그인 페이지 제공

---
+++ src/main/java/sejong/dormitory/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package sejong.dormitory.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/members/login/error")
+                .usernameParameter("email")
+
+                .and()
+
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
