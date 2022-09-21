50546c62aee1d510391abb3f05751772ebec40c1
JongHun <pjh575157@gmail.com>
2022-09-21 11:58:43 +0900

    2022/09/20 MemberController 오타 수정

--- src/main/java/sejong/dormitory/controller/MemberController.java
+++ src/main/java/sejong/dormitory/controller/MemberController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sejong.dormitory.dto.MemberFormDto;
@@ -18,7 +17,7 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/members")
 @RequiredArgsConstructor
-public class MemberContoller {
+public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
