c36bfc1cc7565368331802a388779e08eed60512
bes <pas1007@naver.com>
2022-09-25 03:11:38 +0900

    2022/09/25 findAllByBoardId 추가

--- src/main/java/sejong/dormitory/repository/PhotoRepository.java
+++ src/main/java/sejong/dormitory/repository/PhotoRepository.java
@@ -3,5 +3,8 @@ package sejong.dormitory.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sejong.dormitory.entity.Photo;
 
+import java.util.List;
+
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    List<Photo> findAllByBoardId(Long boardId);
 }
