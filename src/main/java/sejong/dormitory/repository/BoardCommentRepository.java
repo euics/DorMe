7327f8fd6b1c527c2c1b2b0c8dcce57e8239679f
bes <pas1007@naver.com>
2022-09-25 03:04:19 +0900

    2022/09/25 findBoardCommentsByBoardId 추가

--- src/main/java/sejong/dormitory/repository/BoardCommentRepository.java
+++ src/main/java/sejong/dormitory/repository/BoardCommentRepository.java
@@ -3,5 +3,8 @@ package sejong.dormitory.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sejong.dormitory.entity.BoardComment;
 
+import java.util.List;
+
 public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
+    List<BoardComment> findBoardCommentsByBoardId(Long boardId);
 }
