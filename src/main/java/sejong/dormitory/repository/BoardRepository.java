fd0da377516c54837a9610a02d8b75363e95db83
bes <pas1007@naver.com>
2022-09-27 14:01:11 +0900

    2022/09/27 사용하지 않는 import 삭제

--- src/main/java/sejong/dormitory/repository/BoardRepository.java
+++ src/main/java/sejong/dormitory/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sejong.dormitory.entity.Board;
+import sejong.dormitory.entity.Member;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Page<Board> searchByTitleOrContent(String title, String content, Pageable pageable);
