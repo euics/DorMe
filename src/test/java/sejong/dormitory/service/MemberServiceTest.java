de73f603f90a138f8f6816d637c84fbcf4554b73
JongHun <pjh575157@gmail.com>
2022-10-01 13:46:17 +0900

    2022/10/01 MemberServiceTest 수정 - 코드 수정으로 인한 Test 코드 재작성 회원저장/아이디 중복확인/비밀번호 일치여부 Test 코드 작성

--- src/test/java/sejong/dormitory/service/MemberServiceTest.java
+++ src/test/java/sejong/dormitory/service/MemberServiceTest.java
@@ -10,13 +10,14 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import sejong.dormitory.dto.MemberFormDto;
 import sejong.dormitory.entity.Member;
+import sejong.dormitory.repository.MemberRepository;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@TestPropertySource(locations = "classpath:")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class MemberServiceTest {
     @Autowired
     MemberService memberService;
@@ -24,37 +25,61 @@ class MemberServiceTest {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    public Member createMember(){
+    @Autowired
+    MemberRepository memberRepository;
+
+    public MemberFormDto createMember(){
         MemberFormDto memberFormDto = new MemberFormDto();
-        memberFormDto.setUsername("asdf");
-        memberFormDto.setPassword1("asdfasdf1!");
-        memberFormDto.setPassword2("asdfasdf1!");
-        memberFormDto.setSchool("세종대학교");
-        memberFormDto.setLocal("서울");
-        memberFormDto.setNickname("홍길동");
-        return Member.createMember(memberFormDto, passwordEncoder);
+        memberFormDto.setUsername("euics");
+        memberFormDto.setPassword1("1234");
+        memberFormDto.setPassword2("1234");
+
+        return memberFormDto;
+    }
+
+    public MemberFormDto createDifferentPasswordMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setUsername("euics");
+        memberFormDto.setPassword1("1234");
+        memberFormDto.setPassword2("5678");
+
+        return memberFormDto;
     }
 
     @Test
     @DisplayName("회원가입 테스트")
     public void saveMemberTest(){
-        Member member = createMember();
-        Member savedMember = memberService.saveMember(member);
-
-        assertThat(member).isEqualTo(savedMember);
+        MemberFormDto memberFormDto = createMember();
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        Member savedMember = memberRepository.save(member);
+        assertThat(savedMember.getUsername()).isEqualTo(memberFormDto.getUsername());
     }
 
     @Test
     @DisplayName("중복 회원가입 테스트")
-    public void saveDuplicateMemberTest(){
-        Member member1 = createMember();
-        Member member2 = createMember();
-        memberService.saveMember(member1);
+    public void saveDuplicateTest(){
+        MemberFormDto memberFormDto1 = createMember();
+        MemberFormDto memberFormDto2 = createMember();
+        Member member1 = Member.createMember(memberFormDto1, passwordEncoder);
+        Member member2 = Member.createMember(memberFormDto2, passwordEncoder);
+        memberRepository.save(member1);
 
         Throwable e = assertThrows(IllegalStateException.class, () -> {
-            memberService.saveMember(member2);
+            memberService.validateDuplicateMemberFormDto(memberFormDto2);
         });
 
-        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+        assertEquals(e.getMessage(), "이미 가입된 회원입니다.");
+    }
+
+    @Test
+    @DisplayName("비밀번호 중복 테스트")
+    public void passwordDuplicateTest(){
+        MemberFormDto memberFormDto = createDifferentPasswordMember();
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.validateDuplicateMemberFormDto(memberFormDto);
+        });
+
+        assertEquals(e.getMessage(), "비밀번호가 일치하지 않습니다.");
     }
 }
