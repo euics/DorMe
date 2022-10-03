912ff32575f33df7a119f34f2fc4c5020ef964a9
JongHun <pjh575157@gmail.com>
2022-10-03 23:43:57 +0900

    2022/10/03 중복된 Validation message 삭제

--- src/main/java/sejong/dormitory/dto/MemberFormDto.java
+++ src/main/java/sejong/dormitory/dto/MemberFormDto.java
@@ -14,13 +14,11 @@ public class MemberFormDto {
     private String username;
 
     @NotEmpty(message = "비밀번호 입력은 필수입니다.")
-    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+    @Length(min = 8, max = 16, message = "8자 이상, 16자 이하로 입력해주세요")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}", message = "올바르지 않은 형식의 비밀번호입니다.")
     private String password1;
 
     @NotEmpty(message = "비밀번호 확인 입력은 필수입니다.")
-    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}", message = "올바르지 않은 형식의 비밀번호입니다.")
     private String password2;
 
     @NotEmpty(message = "닉네임 입력은 필수입니다.")
