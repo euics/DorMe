c80f9c1c455bfc305bd70cff5ad691e466c41d12
bes <pas1007@naver.com>
2022-10-08 21:37:11 +0900

    2022/10/08 main 홈페이지 컨트롤러 생성

---
+++ src/main/java/sejong/dormitory/controller/HomeController.java
@@ -0,0 +1,14 @@
+package sejong.dormitory.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "MainHome";
+    }
+}
