89aad0c8596d0caae4ff363aabe63c0bbc78b79f
bes <pas1007@naver.com>
2022-12-05 10:11:22 +0900

    2022/12/05 test

--- src/main/java/sejong/dormitory/controller/dormitoryPageController/GyeonggiDoController.java
+++ src/main/java/sejong/dormitory/controller/dormitoryPageController/GyeonggiDoController.java
@@ -1,26 +1,26 @@
-package sejong.dormitory.controller.dormitoryPageController;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import sejong.dormitory.entity.dormitoryPage.GyeonggiDo;
-import sejong.dormitory.service.dormitoryPageService.GyeonggiDoService;
-
-import java.io.IOException;
-import java.util.List;
-
-@Controller
-@RequiredArgsConstructor
-public class GyeonggiDoController {
-    private final GyeonggiDoService gyeonggiDoService;
-
-    @GetMapping("/gyeonggiDo")
-    public String test(Model model) throws IOException{
-        List<GyeonggiDo> gyeonggiDo = gyeonggiDoService.getData1();
-        GyeonggiDo gyeonggiDo2 = gyeonggiDoService.getData();
-        model.addAttribute("img",gyeonggiDo);
-        model.addAttribute("element",gyeonggiDo2);
-        return "dormitoryPage/gyeonggiDo";
-    }
-}
+//package sejong.dormitory.controller.dormitoryPageController;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import sejong.dormitory.entity.dormitoryPage.GyeonggiDo;
+//import sejong.dormitory.service.dormitoryPageService.GyeonggiDoService;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class GyeonggiDoController {
+//    private final GyeonggiDoService gyeonggiDoService;
+//
+//    @GetMapping("/gyeonggiDo")
+//    public String test(Model model) throws IOException{
+//        List<GyeonggiDo> gyeonggiDo = gyeonggiDoService.getData1();
+//        GyeonggiDo gyeonggiDo2 = gyeonggiDoService.getData();
+//        model.addAttribute("img",gyeonggiDo);
+//        model.addAttribute("element",gyeonggiDo2);
+//        return "dormitoryPage/gyeonggiDo";
+//    }
+//}
--- src/main/java/sejong/dormitory/service/dormitoryPageService/GyeonggiDoService.java
+++ src/main/java/sejong/dormitory/service/dormitoryPageService/GyeonggiDoService.java
@@ -1,63 +1,63 @@
-package sejong.dormitory.service.dormitoryPageService;
-
-import lombok.RequiredArgsConstructor;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import sejong.dormitory.entity.dormitoryPage.GyeonggiDo;
-import sejong.dormitory.repository.dormitoryPage.GyeonggiDoRepository;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-@Component
-public class GyeonggiDoService {
-    private static String URL1 = "http://www.gbfh.co.kr/0103/content/facilities/";
-    private static String URL2 = "http://www.gbfh.co.kr/0301/content/dormAbout/";
-    private final GyeonggiDoRepository gyeonggiDoRepository;
-
-    @Transactional
-    public GyeonggiDo getData(){
-        return gyeonggiDoRepository.findTopByOrderByIdDesc();
-    }
-    @Transactional @Scheduled(cron = "0 0 18 * * *")
-    public void getData2() throws IOException{
-        Document doc = Jsoup.connect(URL2).get();
-        String recruitPeriod = doc.selectXpath("/html/body/section/div/div/div/h5[8]/span").text();
-        String price = doc.selectXpath("/html/body/section/div/div/div/h5[12]").text();
-        String condition1 = doc.selectXpath("/html/body/section/div/div/div/h5[3]").text();
-        String condition2 = doc.selectXpath("/html/body/section/div/div/div/h5[4]").text();
-        String recruitMemberInfo = doc.selectXpath("/html/body/section/div/div/div/h5[1]").text();
-
-        GyeonggiDo gyeonggiDo = GyeonggiDo.builder()
-                .recruitPeriod(recruitPeriod)
-                .price(price)
-                .condition1(condition1)
-                .condition2(condition2)
-                .recruitMemberInfo(recruitMemberInfo)
-                .build();
-        gyeonggiDoRepository.save(gyeonggiDo);
-    }
-    @PostConstruct
-    public List<GyeonggiDo> getData1() throws IOException{
-        List<GyeonggiDo> imageSrc = new ArrayList<>();
-        Document doc = Jsoup.connect(URL1).get();
-        Elements elements = doc.select("div.small_image ul li");
-        for(Element element : elements){
-            GyeonggiDo gyeonggiDo = GyeonggiDo.builder()
-                    .detailImagePath("http://www.gbfh.co.kr" + element.select("a img").attr("src"))
-                    .build();
-            imageSrc.add(gyeonggiDo);
-        }
-        return imageSrc;
-    }
-}
+//package sejong.dormitory.service.dormitoryPageService;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//import sejong.dormitory.entity.dormitoryPage.GyeonggiDo;
+//import sejong.dormitory.repository.dormitoryPage.GyeonggiDoRepository;
+//
+//import javax.annotation.PostConstruct;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//@Component
+//public class GyeonggiDoService {
+//    private static String URL1 = "http://www.gbfh.co.kr/0103/content/facilities/";
+//    private static String URL2 = "http://www.gbfh.co.kr/0301/content/dormAbout/";
+//    private final GyeonggiDoRepository gyeonggiDoRepository;
+//
+//    @Transactional
+//    public GyeonggiDo getData(){
+//        return gyeonggiDoRepository.findTopByOrderByIdDesc();
+//    }
+//    @Transactional @Scheduled(cron = "0 0 18 * * *")
+//    public void getData2() throws IOException{
+//        Document doc = Jsoup.connect(URL2).get();
+//        String recruitPeriod = doc.selectXpath("/html/body/section/div/div/div/h5[8]/span").text();
+//        String price = doc.selectXpath("/html/body/section/div/div/div/h5[12]").text();
+//        String condition1 = doc.selectXpath("/html/body/section/div/div/div/h5[3]").text();
+//        String condition2 = doc.selectXpath("/html/body/section/div/div/div/h5[4]").text();
+//        String recruitMemberInfo = doc.selectXpath("/html/body/section/div/div/div/h5[1]").text();
+//
+//        GyeonggiDo gyeonggiDo = GyeonggiDo.builder()
+//                .recruitPeriod(recruitPeriod)
+//                .price(price)
+//                .condition1(condition1)
+//                .condition2(condition2)
+//                .recruitMemberInfo(recruitMemberInfo)
+//                .build();
+//        gyeonggiDoRepository.save(gyeonggiDo);
+//    }
+//    @PostConstruct
+//    public List<GyeonggiDo> getData1() throws IOException{
+//        List<GyeonggiDo> imageSrc = new ArrayList<>();
+//        Document doc = Jsoup.connect(URL1).get();
+//        Elements elements = doc.select("div.small_image ul li");
+//        for(Element element : elements){
+//            GyeonggiDo gyeonggiDo = GyeonggiDo.builder()
+//                    .detailImagePath("http://www.gbfh.co.kr" + element.select("a img").attr("src"))
+//                    .build();
+//            imageSrc.add(gyeonggiDo);
+//        }
+//        return imageSrc;
+//    }
+//}
