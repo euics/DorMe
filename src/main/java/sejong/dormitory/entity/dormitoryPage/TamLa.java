a9c4e780849cacc05d282502a4f690405fb806c9
JongHun <pjh575157@gmail.com>
2022-12-06 19:17:46 +0900

    2022/12/06 시설/정보/이미지 경로 Entity 추가

---
+++ src/main/java/sejong/dormitory/entity/dormitoryPage/TamLa.java
@@ -0,0 +1,28 @@
+package sejong.dormitory.entity.dormitoryPage;
+
+import lombok.*;
+import sejong.dormitory.entity.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class TamLa{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String facility;
+    private String context;
+
+    private String imagePath;
+    private String detailImagePath;
+}
