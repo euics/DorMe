72c16ec2dfc3880d7a82718fb00b5890b68f5a6d
JongHun <pjh575157@gmail.com>
2022-12-07 00:16:03 +0900

    2022/12/07 강원학사-관악 Repository 생성

--- src/main/java/sejong/dormitory/repository/dormitoryPage/GangwonGwanakRepository.java
+++ src/main/java/sejong/dormitory/repository/dormitoryPage/GangwonGwanakRepository.java
@@ -1,9 +1,7 @@
 package sejong.dormitory.repository.dormitoryPage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import sejong.dormitory.entity.dormitoryPage.GangwonDobong;
 import sejong.dormitory.entity.dormitoryPage.GangwonGwanak;
 
 public interface GangwonGwanakRepository extends JpaRepository<GangwonGwanak,Long> {
-    GangwonGwanak findTopByOrderByIdDesc();
 }
