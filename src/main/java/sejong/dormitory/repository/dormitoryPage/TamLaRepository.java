aeb22547465c71ad28e13516ef05cb87384ceca0
JongHun <pjh575157@gmail.com>
2022-12-06 19:19:01 +0900

    2022/12/06 TamLaRepository 추가

---
+++ src/main/java/sejong/dormitory/repository/dormitoryPage/TamLaRepository.java
@@ -0,0 +1,8 @@
+package sejong.dormitory.repository.dormitoryPage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sejong.dormitory.entity.dormitoryPage.TamLa;
+
+public interface TamLaRepository extends JpaRepository<TamLa, Long> {
+}
+
