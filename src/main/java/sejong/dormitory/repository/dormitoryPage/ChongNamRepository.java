7cdf01cad6875e536c14a459dad470901c5d8bc0
JongHun <pjh575157@gmail.com>
2022-12-07 02:05:28 +0900

    2022/12/07 충남학사(서울) Repository 추가

---
+++ src/main/java/sejong/dormitory/repository/dormitoryPage/ChongNamRepository.java
@@ -0,0 +1,7 @@
+package sejong.dormitory.repository.dormitoryPage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sejong.dormitory.entity.dormitoryPage.ChongNam;
+
+public interface ChongNamRepository extends JpaRepository<ChongNam, Long> {
+}
