9aee06415028fc3be5edcb36b9cb1f17f26eeddb
JongHun <pjh575157@gmail.com>
2022-12-07 14:29:10 +0900

    2022/12/07 관원학사(관악) Service시 매 정각 18시에 자동으로 크롤링 할 수 있도록 변경 이후 /gangwonGwanak 웹 사이트 열었을 때 크롤링을 계속하여 발생하는 트래픽 문제를 DB에서 크롤링 정보를 꺼내올 수 있도록 변경

--- src/main/java/sejong/dormitory/service/dormitoryPageService/GangwonGwanakService.java
+++ src/main/java/sejong/dormitory/service/dormitoryPageService/GangwonGwanakService.java
@@ -10,10 +10,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sejong.dormitory.entity.dormitoryPage.GangwonDobong;
-import sejong.dormitory.entity.dormitoryPage.GangwonGwanak;
-import sejong.dormitory.repository.dormitoryPage.GangwonDobongRepository;
-import sejong.dormitory.repository.dormitoryPage.GangwonGwanakRepository;
+import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak0;
+import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak1;
+import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak2;
+import sejong.dormitory.repository.dormitoryPage.GangwonGwanakRepository.GangwonGwanakRepository0;
+import sejong.dormitory.repository.dormitoryPage.GangwonGwanakRepository.GangwonGwanakRepository1;
+import sejong.dormitory.repository.dormitoryPage.GangwonGwanakRepository.GangwonGwanakRepository2;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -27,11 +29,16 @@ import java.util.List;
 public class GangwonGwanakService {
     private final String url0 = "http://injae.gwd.go.kr/injae/gwdormitory/gwanag/facility/facilities";
     private final String baseUrl = "http://injae.gwd.go.kr";
-    private final GangwonGwanakRepository gangwonGwanakRepository;
+    private final GangwonGwanakRepository0 gangwonGwanakRepository0;
+    private final GangwonGwanakRepository1 gangwonGwanakRepository1;
+    private final GangwonGwanakRepository2 gangwonGwanakRepository2;
 
     @PostConstruct @Transactional @Scheduled(cron = "0 0 18 * * *")
-    public List<GangwonGwanak> getDormitoryData0() throws IOException {
-        List<GangwonGwanak> gangwongwanaklist0 = new ArrayList<>();
+    public List<GangwonGwanak0> getDormitoryData0() throws IOException {
+        if(gangwonGwanakRepository0.existsById(1L))
+            gangwonGwanakRepository0.deleteAll();
+
+        List<GangwonGwanak0> gangwongwanaklist0 = new ArrayList<>();
 
         Document doc = Jsoup.connect(url0).get();
         Elements class_content_pack = doc.getElementsByClass("content-pack");
@@ -48,24 +55,24 @@ public class GangwonGwanakService {
             Elements div_ul_li = element.select("div ul li");
 
             if(!div_dl_dd.isEmpty()){
-                GangwonGwanak dormitoryData = GangwonGwanak.builder()
+                GangwonGwanak0 dormitoryData = GangwonGwanak0.builder()
                         .facility0(figure_h3.get(0).text())
                         .context0(div_dl_dd.get(0).text())
                         .imagePath0(image_Url)
                         .detailImagePath0(actual_Url)
                         .build();
                 gangwongwanaklist0.add(dormitoryData);
-                gangwonGwanakRepository.save(dormitoryData);
+                gangwonGwanakRepository0.save(dormitoryData);
             }
             else{
-                GangwonGwanak dormitoryData = GangwonGwanak.builder()
+                GangwonGwanak0 dormitoryData = GangwonGwanak0.builder()
                         .facility0(figure_h3.get(0).text())
                         .context0(div_ul_li.text())
                         .imagePath0(image_Url)
                         .detailImagePath0(actual_Url)
                         .build();
                 gangwongwanaklist0.add(dormitoryData);
-                gangwonGwanakRepository.save(dormitoryData);
+                gangwonGwanakRepository0.save(dormitoryData);
             }
         }
 
@@ -75,8 +82,11 @@ public class GangwonGwanakService {
     private final String url1 = "http://injae.gwd.go.kr/injae/gwdormitory/gwanag/facility/exercise";
 
     @PostConstruct @Transactional @Scheduled(cron = "0 0 18 * * *")
-    public List<GangwonGwanak> getDormitoryData1() throws IOException {
-        List<GangwonGwanak> gangwonGwanakList1 = new ArrayList<>();
+    public List<GangwonGwanak1> getDormitoryData1() throws IOException {
+        if(gangwonGwanakRepository1.existsById(1L))
+            gangwonGwanakRepository1.deleteAll();
+
+        List<GangwonGwanak1> gangwonGwanakList1 = new ArrayList<>();
 
         Document doc = Jsoup.connect(url1).get();
         Elements class_content_pack = doc.getElementsByClass("content-pack");
@@ -90,14 +100,14 @@ public class GangwonGwanakService {
 
             Elements div_dl_dd = element.select("div dl dd");
 
-            GangwonGwanak dormitoryData = GangwonGwanak.builder()
+            GangwonGwanak1 dormitoryData = GangwonGwanak1.builder()
                     .facility1(figure_h3.get(0).text())
                     .context1(div_dl_dd.get(1).text())
                     .imagePath1(image_Url)
                     .detailImagePath1(actual_Url)
                     .build();
             gangwonGwanakList1.add(dormitoryData);
-            gangwonGwanakRepository.save(dormitoryData);
+            gangwonGwanakRepository1.save(dormitoryData);
         }
 
         return gangwonGwanakList1;
@@ -106,8 +116,11 @@ public class GangwonGwanakService {
     private final String url2 = "http://injae.gwd.go.kr/injae/gwdormitory/gwanag/facility/amenities";
 
     @PostConstruct @Transactional @Scheduled(cron = "0 0 18 * * *")
-    public List<GangwonGwanak> getDormitoryData2() throws IOException {
-        List<GangwonGwanak> gangwongwanaklist2 = new ArrayList<>();
+    public List<GangwonGwanak2> getDormitoryData2() throws IOException {
+        if(gangwonGwanakRepository2.existsById(1L))
+            gangwonGwanakRepository2.deleteAll();
+
+        List<GangwonGwanak2> gangwongwanaklist2 = new ArrayList<>();
 
         Document doc = Jsoup.connect(url2).get();
         Elements class_content_pack = doc.getElementsByClass("content-pack");
@@ -124,27 +137,37 @@ public class GangwonGwanakService {
             Elements div_ul_li = element.select("div ul li");
 
             if(!div_dl_dd.isEmpty()){
-                GangwonGwanak dormitoryData = GangwonGwanak.builder()
+                GangwonGwanak2 dormitoryData = GangwonGwanak2.builder()
                         .facility2(figure_h3.get(0).text())
                         .context2(div_dl_dd.get(1).text())
                         .imagePath2(image_Url)
                         .detailImagePath2(actual_Url)
                         .build();
                 gangwongwanaklist2.add(dormitoryData);
-                gangwonGwanakRepository.save(dormitoryData);
+                gangwonGwanakRepository2.save(dormitoryData);
             }
             else{
-                GangwonGwanak dormitoryData = GangwonGwanak.builder()
+                GangwonGwanak2 dormitoryData = GangwonGwanak2.builder()
                         .facility2(figure_h3.get(0).text())
                         .context2(div_ul_li.get(0).text())
                         .imagePath2(image_Url)
                         .detailImagePath2(actual_Url)
                         .build();
                 gangwongwanaklist2.add(dormitoryData);
-                gangwonGwanakRepository.save(dormitoryData);
+                gangwonGwanakRepository2.save(dormitoryData);
             }
         }
 
         return gangwongwanaklist2;
+    }
+
+    public List<GangwonGwanak0> findFromDB0() throws IOException{
+        return gangwonGwanakRepository0.findAll();
+    }
+    public List<GangwonGwanak1> findFromDB1() throws IOException{
+        return gangwonGwanakRepository1.findAll();
+    }
+    public List<GangwonGwanak2> findFromDB2() throws IOException{
+        return gangwonGwanakRepository2.findAll();
     }
 }
