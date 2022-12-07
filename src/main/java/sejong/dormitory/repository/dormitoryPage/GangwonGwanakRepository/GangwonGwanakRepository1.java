1c1d1f0ba1290eefd5b00c0316d1005ecbb78d5c
JongHun <pjh575157@gmail.com>
2022-12-07 15:30:21 +0900

    2022/12/07 불필요 DB 삭제

--- src/main/java/sejong/dormitory/repository/dormitoryPage/GangwonGwanakRepository/GangwonGwanakRepository1.java
+++
@@ -1,7 +0,0 @@
-package sejong.dormitory.repository.dormitoryPage.GangwonGwanakRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak1;
-
-public interface GangwonGwanakRepository1 extends JpaRepository<GangwonGwanak1,Long> {
-}
--- src/main/java/sejong/dormitory/repository/dormitoryPage/GangwonGwanakRepository/GangwonGwanakRepository2.java
+++
@@ -1,7 +0,0 @@
-package sejong.dormitory.repository.dormitoryPage.GangwonGwanakRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak2;
-
-public interface GangwonGwanakRepository2 extends JpaRepository<GangwonGwanak2,Long> {
-}
