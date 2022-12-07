f6929beedfd25a0877db698b4e68c5dd1a8c9c88
JongHun <pjh575157@gmail.com>
2022-12-07 14:28:02 +0900

    2022/12/07 관원학사(관악) Repository 분리

--- src/main/java/sejong/dormitory/repository/dormitoryPage/GangwonGwanakRepository.java
+++
@@ -1,7 +0,0 @@
-package sejong.dormitory.repository.dormitoryPage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sejong.dormitory.entity.dormitoryPage.GangwonGwanak;
-
-public interface GangwonGwanakRepository extends JpaRepository<GangwonGwanak,Long> {
-}
