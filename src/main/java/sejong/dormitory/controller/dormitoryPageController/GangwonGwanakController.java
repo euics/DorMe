a78113578c0b3e75c20436cb1d94bf0718e837e4
JongHun <pjh575157@gmail.com>
2022-12-07 14:30:36 +0900

    2022/12/07 관원학사(관악) Controller 변경

--- src/main/java/sejong/dormitory/controller/dormitoryPageController/GangwonGwanakController.java
+++ src/main/java/sejong/dormitory/controller/dormitoryPageController/GangwonGwanakController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import sejong.dormitory.entity.dormitoryPage.GangwonGwanak;
+import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak0;
+import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak1;
+import sejong.dormitory.entity.dormitoryPage.gangwongwanak.GangwonGwanak2;
 import sejong.dormitory.service.dormitoryPageService.GangwonGwanakService;
 
 import java.io.IOException;
@@ -17,9 +19,9 @@ public class GangwonGwanakController {
 
     @GetMapping("/gangwonGwanak")
     public String gangwonGwanak(Model model) throws IOException {
-        List<GangwonGwanak> dormitoryData0 = gangwonGwanakService.getDormitoryData0();
-        List<GangwonGwanak> dormitoryData1 = gangwonGwanakService.getDormitoryData1();
-        List<GangwonGwanak> dormitoryData2 = gangwonGwanakService.getDormitoryData2();
+        List<GangwonGwanak0> dormitoryData0 = gangwonGwanakService.findFromDB0();
+        List<GangwonGwanak1> dormitoryData1 = gangwonGwanakService.findFromDB1();
+        List<GangwonGwanak2> dormitoryData2 = gangwonGwanakService.findFromDB2();
         model.addAttribute("gangwonGwanakData0", dormitoryData0);
         model.addAttribute("gangwonGwanakData1", dormitoryData1);
         model.addAttribute("gangwonGwanakData2", dormitoryData2);
